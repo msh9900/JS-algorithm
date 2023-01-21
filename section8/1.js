34289326d91e28b98dccb9f0838cb3f7fbc6f5c4
MOS <msh9900@naver.com>
2023-01-21 19:33:50 +0900

    섹션 789

---
+++ section8/1.js
@@ -0,0 +1,13 @@
+// 재귀함수
+function solution(n) {
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+      console.log(L);
+    }
+  }
+  DFS(n);
+}
+
+solution(3);
---
+++ section8/3.js
@@ -0,0 +1,16 @@
+// 이진트리순회
+function solution(n) {
+  let answer = "";
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      answer += v + " ";
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+    }
+  }
+  DFS(n);
+  return answer;
+}
+
+console.log(solution(1));
